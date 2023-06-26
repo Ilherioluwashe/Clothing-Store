68c681a497765e069f114fffd40a478f542e5708
Michael Akingbade <Akingbademichael7@gmail.com>
2023-06-26 04:00:44 +0100

    created authentication page componenet, along side sign in form component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,19 @@
 // import { useEffect } from "react";
 // import { getRedirectResult } from "firebase/auth";
 
-import {
-  auth,
-  signInWithGooglePopup,
-  // signInWithGoogleRedirect,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+// import {
+//   auth,
+//   signInWithGooglePopup,
+//   // signInWithGoogleRedirect,
+//   createUserDocumentFromAuth,
+// } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
+import "./authentication.styles.scss";
+
+const Authentication = () => {
   // useEffect(() => {
   //   async function fetchData() {
   //     const response = await getRedirectResult(auth);
@@ -21,15 +24,10 @@ const SignIn = () => {
   //   fetchData();
   // }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>sign in with Google Popup</button>
+    <div className="authentication-container">
+      {/* <button onClick={logGoogleUser}>sign in with Google Popup</button> */}
+      <SignInForm />
       <SignUpForm />
       {/* <button onClick={signInWithGoogleRedirect}>
         sign in with Google Redirect
@@ -38,4 +36,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Authentication;
